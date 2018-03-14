71e5d60f1b04fe109e92dbbab59dc1c77344dd56
v_yuanjiankai <v_yuanjiankai@baidu.com>
2018-03-14 23:57:48 +0800

    增加websocket sockjs 内容

---
+++ src/main/java/WebSocket/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package WebSocket;
+
+import interceptor.HandshakeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+
+    @Autowired
+    private WebSocketTest2 webSocketTest2;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(webSocketTest2, "/websocket2").withSockJS();
+    }
+
+}
